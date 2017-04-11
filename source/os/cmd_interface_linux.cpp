bb746325f741e2ef20e3b833ede01d87bb8b9584
郭盖华 <ggh@imscv.com>
2017-04-11 13:54:35 +0800

    Add linux com port driver

--- source/os/cmd_interface_linux.cpp
+++ source/os/cmd_interface_linux.cpp
@@ -1,12 +1,106 @@
-#include "cmd_interface_win.h"
 
+#include "cmd_interface_linux.h"
 
+#include <sys/file.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <string.h>
+#include <memory.h>
 
-WinSerialPort::WinSerialPort()
+CmdInterfaceLinux::CmdInterfaceLinux()
 {
 }
 
-
-WinSerialPort::~WinSerialPort()
+CmdInterfaceLinux::~CmdInterfaceLinux()
 {
+}
+
+bool CmdInterfaceLinux::Open(string & port_name)
+{
+	int flags = (O_RDWR | O_NOCTTY | O_NONBLOCK | O_CLOEXEC | O_SYNC);
+	mComHandle = open(port_name.c_str(), flags);
+	if (-1 == mComHandle) {
+		return -1;
+	}
+
+	if (-1 == fcntl(mComHandle, F_SETFD, FD_CLOEXEC)) {
+		Close();
+		return -1;
+	}
+
+	// get port options
+	struct termios options;
+	if (-1 == tcgetattr(mComHandle, &options)) {
+		Close();
+		return -1;
+	}
+
+	// Get port configuration for modification
+	tcgetattr(mComHandle, &options);
+	options.c_iflag = IGNPAR;
+	options.c_cflag &= ~CSIZE;
+	options.c_cflag |= CS8;
+	options.c_cflag |= CRTSCTS;
+	options.c_cflag &= ~PARENB;	//disable parity
+	options.c_cflag &= ~CSTOPB;	//1 stop bit
+	options.c_cflag |= CLOCAL;  // ignore status lines
+	options.c_cflag |= CREAD;   // enable receiver
+	options.c_oflag = 0;
+	options.c_lflag = 0;  // ICANON;
+	options.c_cc[VMIN] = 24;
+	options.c_cc[VTIME] = 1;
+
+	tcflush(mComHandle, TCIFLUSH);
+
+	tcsetattr(mComHandle, TCSANOW, &options);
+
+	if (-1 == flock(mComHandle, LOCK_EX | LOCK_NB)) {
+		Close();
+		return false;
+	}
+
+	mIsOpened = true;
+	return true;
+}
+
+bool CmdInterfaceLinux::Close()
+{
+	if (mComHandle != -1) {
+		close(mComHandle);
+		mComHandle = -1;
+	}
+
+	if (mRxThread->joinable())
+		mRxThread->join();
+
+	delete mRxThread;
+	mRxThread = NULL;
+	return true;
+}
+
+bool CmdInterfaceLinux::GetUvcRelatedCmdPort(string & uvc_port_name, string & cmd_port_name)
+{
+	return false;
+}
+
+bool CmdInterfaceLinux::ReadFromIO(uint8_t * rx_buf, uint32_t rx_buf_len, uint32_t * rx_len)
+{
+	if (IsOpened()) {
+		int32_t len = read(mComHandle, rx_buf, rx_buf_len);
+		if (rx_len)
+			*rx_len = len;
+	}	
+	return true;
+}
+
+bool CmdInterfaceLinux::WriteToIo(const uint8_t * tx_buf, uint32_t tx_buf_len, uint32_t * tx_len)
+{
+	if (IsOpened()) {
+		int32_t len = write(mComHandle, tx_buf, tx_buf_len);
+		if (tx_len)
+			*tx_len = len;
+	}
+	return false;
 }
--- source/os/cmd_interface_linux.h
+++ source/os/cmd_interface_linux.h
@@ -2,15 +2,24 @@
 #ifndef __WIN_SERIAL_PORT_H__
 #define __WIN_SERIAL_PORT_H__
 
-#include "../../cmd_interface.h"
+#include "cmd_interface.h"
 
-class CmdInterfaceWin:public CmdInterface
+class CmdInterfaceLinux:public CmdInterface
 {
 public:
-	CmdInterfaceWin();
-	~CmdInterfaceWin();
+	CmdInterfaceLinux();
+	~CmdInterfaceLinux();
 
 
+	// Inherited via CmdInterface
+	virtual bool Open(string & port_name) override;
+	virtual bool Close() override;
+	virtual bool GetUvcRelatedCmdPort(string & uvc_port_name, string & cmd_port_name) override;
+	virtual bool ReadFromIO(uint8_t * rx_buf, uint32_t rx_buf_len, uint32_t * rx_len) override;
+	virtual bool WriteToIo(const uint8_t * tx_buf, uint32_t tx_buf_len, uint32_t * tx_len) override;
+
+private:
+    int32_t mComHandle;
 };
 
 #endif
